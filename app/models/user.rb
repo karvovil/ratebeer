c72b45ef707848557fccc8ecf6f98eeb2c737cc2
Ville Karvonen <villekarvonen1@gmail.com>
2023-09-17 18:06:16 +0300

    beer_club and associations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   include RatingAverage
 
+  has_many :memberships, dependent: :destroy
+  has_many :beer_clubs, through: :memberships
+  has_many :ratings
+  has_many :beers, through: :ratings
+
   validates :username, uniqueness: true
   validates :username, length: { minimum: 3, maximum: 30 }
-
-  has_many :ratings
 end
