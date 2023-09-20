5a16a68da1d8bb48def93637db22a70c3d38a26a
Ville Karvonen <villekarvonen1@gmail.com>
2023-09-20 14:29:25 +0300

    removed permissions from users nonusers

--- spec/features/beer_page_spec.rb
+++ spec/features/beer_page_spec.rb
@@ -4,26 +4,33 @@ include Helpers
 
 describe "Beer" do
   let!(:brewery) { FactoryBot.create :brewery, name: "Koff" }
+  let!(:user) { FactoryBot.create :user }
 
-  it "when name is valid, can be added" do
-    visit new_beer_path
-    select('Koff', from: 'beer[brewery_id]')
-    select('IPA', from: 'beer[style]')
-    fill_in('beer[name]', with: 'Koffin ipa')
+  describe "when logged in" do
+    before :each do
+      sign_in(username: "Pekka", password: "Foobar1")
+    end
 
-    expect{
+    it "when name is valid, can be added" do
+      visit new_beer_path
+      select('Koff', from: 'beer[brewery_id]')
+      select('IPA', from: 'beer[style]')
+      fill_in('beer[name]', with: 'Koffin ipa')
+
+      expect{
+        click_button "Create Beer"
+      }.to change{Beer.count}.from(0).to(1)
+    end
+
+    it "with nonvalid name, can not be added" do
+      visit new_beer_path
+      select('Koff', from: 'beer[brewery_id]')
+      select('IPA', from: 'beer[style]')
+      fill_in('beer[name]', with: '')
       click_button "Create Beer"
-    }.to change{Beer.count}.from(0).to(1)
-  end
 
-  it "with nonvalid name, can not be added" do
-    visit new_beer_path
-    select('Koff', from: 'beer[brewery_id]')
-    select('IPA', from: 'beer[style]')
-    fill_in('beer[name]', with: '')
-    click_button "Create Beer"
-
-    expect(Beer.count).to eq(0)
-    expect(page).to have_content "Name can't be blank"
+      expect(Beer.count).to eq(0)
+      expect(page).to have_content "Name can't be blank"
+    end
   end
 end
