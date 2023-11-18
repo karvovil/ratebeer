45a86d8b2225dc2488d3fc1dbf2e588b94b9f0f6
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-18 08:33:57 +0200

    dynamic update  messages with action cable

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :user
+
+  after_create_commit do 
+    broadcast_prepend_to "messages_index", partial: "messages/message", target: "messages"
+  end
+
 end
+
