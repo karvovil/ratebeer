002a3cd0e3e773f8f4b0dc9a35d773c210fdd400
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-27 13:29:50 +0200

    new brewery form fields can be reset with a button

--- app/javascript/controllers/newbrew_controller.js
+++ app/javascript/controllers/newbrew_controller.js
@@ -1,8 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-   // We'll talk about the connect in a second... 
+   static targets = ["name", "year", "active"];
+
    connect() {
       console.log("Hello, Stimulus!");
+   }
+
+   reset(event) {
+      event.preventDefault();
+      this.nameTarget.value = '';
+      this.activeTarget.checked = false;
+      this.yearTarget.value = '';
    }
 }
